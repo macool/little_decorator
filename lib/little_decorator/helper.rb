32427367d37317d43dbbbc40b242ef993f279e54
Mario Andrés <a@macool.me>
2015-03-21 18:37:20 -0500

    allow decorator as second argument to #decorate

--- lib/little_decorator/helper.rb
+++ lib/little_decorator/helper.rb
@@ -1,17 +1,19 @@
 class LittleDecorator
   module Helper
-
-    def decorate(item_or_collection)
+    def decorate(item_or_collection, decorator=nil)
       if item_or_collection.respond_to?(:map)
-        item_or_collection.map{ |item| decorate(item) }
+        item_or_collection.map do |item|
+          decorate(item, decorator)
+        end
       else
         item = item_or_collection
         return item if LittleDecorator === item
-        decorator = "#{item.class}Decorator".constantize
+        if decorator.nil?
+          decorator = "#{item.class}Decorator".constantize
+        end
         decorator.new(item, self)
       end
     end
     alias_method :d, :decorate
-
   end
 end
